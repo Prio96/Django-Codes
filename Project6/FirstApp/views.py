56d5fafd9fd7676390415299cda09b2d61f84ee4
Prio <prio96@icloud.com>
2024-08-28 03:18:10 +0600

    Added and deleted database records from frontend

--- Project6/FirstApp/views.py
+++ Project6/FirstApp/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
+from .models import Student
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    student=Student.objects.all()
+    print(student)
+    return render(request,'home.html',{'data':student})
+
+def delete_student(request,roll):
+    Student.objects.get(pk=roll).delete()
+    return redirect("homepage")
+    
+    
