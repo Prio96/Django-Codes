5ffd9f082e775d76f9045b980de7c8e44af96b0c
Prio <prio96@icloud.com>
2024-08-30 15:04:50 +0600

    Added Base Template

--- Blog_Project/Blog_Project/settings.py
+++ Blog_Project/Blog_Project/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'Blog_Project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
