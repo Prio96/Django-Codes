40dacaf1a216bcb27621684bfdc5c92dfd611e72
Prio <prio96@icloud.com>
2024-10-20 11:46:49 +0600

    Learned About Cookies and Session

--- Project9/FirstApp/urls.py
+++ Project9/FirstApp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import home,get_cookie,del_cookie
+from .views import home,get_cookie,del_cookie,set_session,get_session,del_session
 urlpatterns = [
     path('',home),
     path('get/',get_cookie),
-    path('del/',del_cookie)
+    path('del/',del_cookie),
+    path('set_session/',set_session),
+    path('get_session/',get_session),
+    path('del_session/',del_session)
 ]
--- Project9/FirstApp/views.py
+++ Project9/FirstApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from datetime import datetime,timedelta
+from django.http import HttpResponse
 # Create your views here.
 def home(request):
     response=render(request,"home.html")
@@ -16,4 +17,32 @@ def del_cookie(request):
     response=render(request,"del_cookie.html")
     response.delete_cookie('name')
     return response
+
+def set_session(request):
+    data={
+        'name':'Karim',
+        'age':15,
+        'nationality':'Indian'
+    }
+    request.session.update(data)
+    print(request.session.get_session_cookie_age())
+    print(request.session.get_expiry_date())
+    return render(request,'set_session.html')
+
+def get_session(request):
+    if 'name' in request.session and 'age' in request.session and 'nationality' in request.session:
+        name=request.session.get('name','Guest')
+        age=request.session.get('age',"Age unknown")
+        nationality=request.session.get('nationality')
+        request.session.modified=True
+        return render(request,'get_session.html',{'name':name,'age':age,'nationality':nationality})
+    else:
+        return HttpResponse("Your session has expired. Log In Again")
+
+def del_session(request):
+    # del request.session['name']
+    # del request.session['age']
+    request.session.flush()
+    return render(request,'del_session.html')
+    
     
