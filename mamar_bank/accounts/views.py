be8ae5c4056316aad99c25156768028588e7d1b3
Prio <prio96@icloud.com>
2024-12-26 16:18:46 +0600

    Started Module 22

--- mamar_bank/accounts/views.py
+++ mamar_bank/accounts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views.generic import FormView
-from .forms import UserRegistrationForm
+from django.views import View
+from .forms import UserRegistrationForm,UserUpdateForm
 from django.contrib.auth import login,logout
 from django.contrib.auth.views import LoginView,LogoutView
 from django.urls import reverse_lazy
@@ -25,9 +26,23 @@ class UserLoginView(LoginView):
         return reverse_lazy('Home')
     
 
-class UserLogoutView(LogoutView):
+class UserLogoutView(LogoutView):#Problem
     def get_success_url(self):
         if self.request.user.is_authenticated:
             logout(self.request)
         
         return reverse_lazy('Home')
+    
+class UserBankAccountUpdateView(View):
+    template_name = 'accounts/profile.html'
+
+    def get(self, request):
+        form = UserUpdateForm(instance=request.user)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = UserUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('Profile')  # Redirect to the user's profile page
+        return render(request, self.template_name, {'form': form})
